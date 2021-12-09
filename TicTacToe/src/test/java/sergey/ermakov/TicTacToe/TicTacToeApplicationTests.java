537f1ecb9f6a3002fae2b5d5e43e0fcb68375853
sergeyermakov239 <sergeyermakov13112004@gmail.com>
2021-12-09 22:24:51 +0300

    committttttt

---
+++ TicTacToe/src/test/java/sergey/ermakov/TicTacToe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package sergey.ermakov.TicTacToe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
